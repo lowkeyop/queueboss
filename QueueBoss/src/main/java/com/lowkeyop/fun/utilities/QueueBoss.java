ba2c70304401a3f96215034e08dd266643397c65
lowkeyop <ckennerly9@gmail.com>
2018-02-10 17:06:29 -0500

    Iniital Base Project Check-In

---
+++ QueueBoss/src/main/java/com/lowkeyop/fun/utilities/QueueBoss.java
@@ -0,0 +1,10 @@
+package com.lowkeyop.fun.utilities;
+
+public class QueueBoss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("functionally works");
+	}
+
+}
