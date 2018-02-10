13f637e68c0e61d62607c899f56f19a85932f12a
lowkeyop <ckennerly9@gmail.com>
2018-02-10 17:31:53 -0500

    Creating List interface for all Queue-ing list types for games

---
+++ QueueBoss/src/main/java/com/lowkeyop/fun/utilities/models/List.java
@@ -0,0 +1,8 @@
+package com.lowkeyop.fun.utilities.models;
+
+public interface List {
+	void addPlayerToList(Player p);
+	void sendPlayerToTable(Player p, int tableNumber);
+	void holdPlayerPosition(Player p, int position);
+	//TODO: Skip player if currently in match
+}
